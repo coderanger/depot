1f97645b9cc9055debf289019b3a3f154119f69b
Noah Kantrowitz <noah@coderanger.net>
2013-12-13 14:07:19 -0800

    Bump version for release.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'depot',
-    version = '0.0.2',
+    version = '0.0.3',
     packages = find_packages(),
     author = 'Noah Kantrowitz',
     author_email = 'noah@coderanger.net',
