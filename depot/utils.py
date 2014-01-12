55f05b3e5ba8e7d9264b9b230ff43d1067165d10
Noah Kantrowitz <noah@coderanger.net>
2014-01-12 15:58:13 -0800

    Derp, six already does this.

--- depot/utils.py
+++ depot/utils.py
@@ -1,13 +1,28 @@
+#
+# Author:: Noah Kantrowitz <noah@coderanger.net>
+#
+# Copyright 2014, Noah Kantrowitz
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 import gzip
 
-try:
-    from cStringIO import StringIO
-except ImportError:
-    from StringIO import StringIO
+import six
 
 
 def gzip_compress(data, filename='<data>'):
-    gz_data = StringIO()
+    gz_data = six.BytesIO()
     gz = gzip.GzipFile(filename, 'wb', fileobj=gz_data)
     gz.write(data)
     gz.close()
