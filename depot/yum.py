1fea73c26873bbe4e7ad1c84a0fee3b49b97665e
Noah Kantrowitz <noah@coderanger.net>
2014-01-12 02:42:56 -0800

    Parser for primary.xml

--- depot/yum.py
+++ depot/yum.py
@@ -5,6 +5,28 @@ from lxml.builder import ElementMaker
 from lxml.etree import QName
 import six
 
+class YumMeta(collections.OrderedDict):
+    nsmap = {}
+
+    @classmethod
+    def from_file(cls, filename, fileobj=None):
+        fileobj = fileobj or open(filename, 'rb')
+        return cls.from_element(lxml.parse(fileobj))
+
+    @classmethod
+    def from_element(cls, root):
+        raise NotImplementedError
+
+    def to_element(self, E):
+        raise NotImplementedError
+
+    def __str__(self):
+        return lxml.tostring(
+            self.to_element(ElementMaker(nsmap=self.nsmap)),
+            xml_declaration=True,
+            encoding='UTF-8')
+
+
 class YumRepoMDData(collections.OrderedDict):
     def __init__(self, type, *args, **kwargs):
         super(YumRepoMDData, self).__init__(*args, **kwargs)
@@ -30,18 +52,17 @@ class YumRepoMDData(collections.OrderedDict):
         return E.data(*sub, type=self.type)
 
 
-class YumRepoMD(collections.OrderedDict):
+class YumRepoMD(YumMeta):
     DataClass = YumRepoMDData
+    nsmap = {
+        None: 'http://linux.duke.edu/metadata/repo',
+        'rpm': 'http://linux.duke.edu/metadata/rpm',
+    }
 
     def __init__(self, revision=0, tags=None, *args, **kwargs):
         super(YumRepoMD, self).__init__(*args, **kwargs)
         self.revision = revision
         self.tags = tags or []
-
-    @classmethod
-    def from_file(cls, filename, fileobj=None):
-        fileobj = fileobj or open(filename, 'rb')
-        return cls.from_element(lxml.parse(fileobj))
 
     @classmethod
     def from_element(cls, root):
@@ -55,16 +76,125 @@ class YumRepoMD(collections.OrderedDict):
         return self
 
     def to_element(self, E):
-        return E.repomd(
-            E.revision(str(self.revision)),
-            E.tags(*[E.content(tag) for tag in self.tags]),
-            *[data.to_element(E) for data in six.itervalues(self)]
-        )
+        sub = [E.revision(str(self.revision))]
+        if self.tags:
+            sub.append(E.tags(*[E.content(tag) for tag in self.tags]))
+        for data in six.itervalues(self):
+            sub.append(data.to_element(E))
+        return E.repomd(*sub)
 
-    def __str__(self):
-        nsmap = {
-            None: 'http://linux.duke.edu/metadata/repo',
-            'rpm': 'http://linux.duke.edu/metadata/rpm',
-        }
-        E = ElementMaker(nsmap=nsmap)
-        return lxml.tostring(self.to_element(E), xml_declaration=True, encoding='UTF-8')
+
+class YumPrimaryFormat(collections.OrderedDict):
+    def __init__(self, *args, **kwargs):
+        super(YumPrimaryFormat, self).__init__(*args, **kwargs)
+        self.files = []
+
+    @classmethod
+    def from_element(cls, root):
+        self = cls()
+        for elm in root.findall('*'):
+            key = QName(elm.tag).localname
+            if key == 'header-range':
+                self[key] = elm.attrib
+            elif key == 'provides' or key == 'requires' or key == 'conflicts' or key == 'obsoletes':
+                self[key] = [entry.attrib for entry in elm.findall('{*}entry')]
+            elif key == 'file':
+                self.files.append((elm.attrib.get('type'), elm.text))
+            else:
+                self[key] = elm.text
+        return self
+
+    def to_element(self, E):
+        sub = []
+        for key, value in six.iteritems(self):
+            ns_key = '{{http://linux.duke.edu/metadata/rpm}}{0}'.format(key)
+            if key == 'header-range':
+                elm = E(ns_key)
+                for a_key, a_value in six.iteritems(value):
+                    elm.attrib[a_key] = a_value
+            elif key == 'provides' or key == 'requires' or key == 'conflicts' or key == 'obsoletes':
+                entries = []
+                for entry in value:
+                    e_elm = E('{http://linux.duke.edu/metadata/rpm}entry')
+                    for e_key, e_value in six.iteritems(entry):
+                        e_elm.attrib[e_key] = e_value
+                    entries.append(e_elm)
+                if not entries:
+                    entries.append('')
+                elm = E(ns_key, *entries)
+            elif value:
+                elm = E(ns_key, value)
+            else:
+                elm = E(ns_key)
+            sub.append(elm)
+        for file_type, file_path in self.files:
+            elm = E.file(file_path)
+            if file_type:
+                elm.attrib['type'] = file_type
+            sub.append(elm)
+        return E.format(*sub)
+
+
+class YumPrimaryPackage(collections.OrderedDict):
+    FormatClass = YumPrimaryFormat
+
+    def __init__(self, type, *args, **kwargs):
+        super(YumPrimaryPackage, self).__init__(*args, **kwargs)
+        self.type = type
+
+    @classmethod
+    def from_element(cls, root):
+        self = cls(root.attrib['type'])
+        for elm in root.findall('*'):
+            key = QName(elm.tag).localname
+            if key == 'version' or key == 'time' or key == 'size':
+                self[key] = elm.attrib
+            elif key == 'location':
+                self[key] = elm.attrib['href']
+            elif key == 'format':
+                self[key] = self.FormatClass.from_element(elm)
+            else:
+                self[key] = elm.text
+        return self
+
+    def full_version(self):
+        return '{epoch}:{ver}={rel}'.format(**self['version'])
+
+    def to_element(self, E):
+        sub = []
+        for key, value in six.iteritems(self):
+            if key == 'version' or key == 'time' or key == 'size':
+                elm = E(key)
+                for a_key, a_value in six.iteritems(value):
+                    elm.attrib[a_key] = a_value
+            elif key == 'location':
+                elm = E.location(href=value)
+            elif key == 'checksum':
+                elm = E.checksum(value)
+                elm.attrib['type'] = 'sha'
+                elm.attrib['pkgid'] = 'YES'
+            elif key == 'format':
+                elm = value.to_element(E)
+            else:
+                elm = E(key, value or '')
+            sub.append(elm)
+        return E.package(*sub, type=self.type)
+
+
+class YumPrimary(YumMeta):
+    PackageClass = YumPrimaryPackage
+    nsmap = {
+        None: 'http://linux.duke.edu/metadata/common',
+        'rpm': 'http://linux.duke.edu/metadata/rpm',
+    }
+
+    @classmethod
+    def from_element(cls, root):
+        self = cls()
+        for elm in root.findall('{*}package'):
+            pkg = self.PackageClass.from_element(elm)
+            self[(pkg['name'], pkg['arch'], pkg.full_version())] = pkg
+        return self
+
+    def to_element(self, E):
+        return E.metadata(*[pkg.to_element(E) for pkg in six.itervalues(self)], packages=str(len(self)))
--- test/test_yum.py
+++ test/test_yum.py
@@ -4,12 +4,20 @@ import re
 import pytest
 from pretend import call_recorder, call, stub
 
-from depot.yum import YumRepoMD
+from depot.yum import YumRepoMD, YumPrimary
+
+# Convert XML into a format that diffs nicely
+def unify_spacing(data):
+  return re.sub('>', '>\n', re.sub('\s+', ' ', re.sub('>', '>\n', re.sub('\n', '', data))))
 
 class TestYumRepoMD(object):
   @pytest.fixture
   def epel(self):
     return YumRepoMD.from_file(os.path.join(os.path.dirname(__file__), 'data', 'epel_repomd.xml'))
+
+  @pytest.fixture
+  def pgdg(self):
+    return YumRepoMD.from_file(os.path.join(os.path.dirname(__file__), 'data', 'pgdg_repomd.xml'))
 
   def test_from_file(self, epel):
     assert epel.revision == 1389466441
@@ -18,8 +26,35 @@ class TestYumRepoMD(object):
     assert epel['filelists']['open-checksum'] == 'b6081d9789bf5cef2ffac8c03fa67224b8c22f53'
     assert epel['group']['location'] == 'repodata/fdddf90da3a700ad6da5ff78e13c17258655bbe3-comps-el5.xml'
 
-  def test_str(self, epel):
+  def test_str_epel(self, epel):
     raw = open(os.path.join(os.path.dirname(__file__), 'data', 'epel_repomd.xml'), 'rb').read()
-    def unify_spacing(data):
-      return re.sub('>', '>\n', re.sub('\s+', ' ', re.sub('>', '>\n', re.sub('\n', '', data))))
     assert unify_spacing(str(epel)) == unify_spacing(raw)
+
+  def test_str_pgdg(self, pgdg):
+    raw = open(os.path.join(os.path.dirname(__file__), 'data', 'pgdg_repomd.xml'), 'rb').read()
+    assert unify_spacing(str(pgdg)) == unify_spacing(raw)
+
+
+class TestYumPrimary(object):
+  @pytest.fixture
+  def epel(self):
+    return YumPrimary.from_file(os.path.join(os.path.dirname(__file__), 'data', 'epel_primary.xml'))
+
+  @pytest.fixture
+  def pgdg(self):
+    return YumPrimary.from_file(os.path.join(os.path.dirname(__file__), 'data', 'pgdg_primary.xml'))
+
+  def test_str_epel(self, epel):
+    raw = open(os.path.join(os.path.dirname(__file__), 'data', 'epel_primary.xml'), 'rb').read()
+    assert unify_spacing(str(epel)) == unify_spacing(raw)
+
+  def test_str_pgdg(self, pgdg):
+    raw = open(os.path.join(os.path.dirname(__file__), 'data', 'pgdg_primary.xml'), 'rb').read()
+    assert unify_spacing(str(pgdg)) == unify_spacing(raw)
+
+  def test_str_pgdgmini(self):
+    path = os.path.join(os.path.dirname(__file__), 'data', 'pgdgmini_primary.xml')
+    pgdg = YumPrimary.from_file(path)
+    raw = open(path, 'rb').read()
+    assert unify_spacing(str(pgdg)) == unify_spacing(raw)
+
