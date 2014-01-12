14081691383c92edf70773faa3bd9ba0418f28d7
Noah Kantrowitz <noah@coderanger.net>
2014-01-12 15:39:58 -0800

    Handle Python 3.

--- depot/utils.py
+++ depot/utils.py
@@ -1,9 +1,13 @@
-import cStringIO
 import gzip
+
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from StringIO import StringIO
 
 
 def gzip_compress(data, filename='<data>'):
-    gz_data = cStringIO.StringIO()
+    gz_data = StringIO()
     gz = gzip.GzipFile(filename, 'wb', fileobj=gz_data)
     gz.write(data)
     gz.close()
