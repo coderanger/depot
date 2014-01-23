70d5a0beacf59dbac361a1792abbe662e1bf1a4b
Noah Kantrowitz <noah@coderanger.net>
2014-01-23 12:38:44 -0800

    Bump version for release.

--- depot/version.py
+++ depot/version.py
@@ -1,2 +1,2 @@
-__version_info__ = (0, 0, 9, 'dev')
+__version_info__ = (0, 0, 10)
 __version__ = '.'.join(map(str, __version_info__))
--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ class PyTest(TestCommand):
 
 setup(
     name = 'depot',
-    version = '0.0.9-dev',
+    version = '0.0.10',
     packages = find_packages(),
     author = 'Noah Kantrowitz',
     author_email = 'noah@coderanger.net',
