ae41d8170e54b84a89f6a68ce735f009ff2e21fa
Noah Kantrowitz <noah@coderanger.net>
2014-01-23 12:18:58 -0800

    Not pre-alpha anymore.

--- setup.py
+++ setup.py
@@ -31,9 +31,9 @@ setup(
     url = 'http://github.com/coderanger/depot',
     classifiers = [
         #'Development Status :: 1 - Planning',
-        'Development Status :: 2 - Pre-Alpha',
+        #'Development Status :: 2 - Pre-Alpha',
         #'Development Status :: 3 - Alpha',
-        #'Development Status :: 4 - Beta',
+        'Development Status :: 4 - Beta',
         #'Development Status :: 5 - Production/Stable',
         #'Development Status :: 6 - Mature',
         #'Development Status :: 7 - Inactive',
