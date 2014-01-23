3036ddb60b8b306091f22e8966cf715a5a2725a3
Noah Kantrowitz <noah@coderanger.net>
2014-01-23 12:18:47 -0800

    Relicense.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     author_email = 'noah@coderanger.net',
     description = 'Manage package repositories in the cloud.',
     long_description = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
-    license = 'BSD',
+    license = 'Apache 2.0',
     keywords = '',
     url = 'http://github.com/coderanger/depot',
     classifiers = [
@@ -37,7 +37,7 @@ setup(
         #'Development Status :: 5 - Production/Stable',
         #'Development Status :: 6 - Mature',
         #'Development Status :: 7 - Inactive',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
