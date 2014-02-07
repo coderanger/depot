311a877b7cc9684b0c2f76fef87c22edfc2aefe9
Noah Kantrowitz <noah@coderanger.net>
2014-02-07 15:30:00 -0800

    Bump version for release.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ class PyTest(TestCommand):
 
 setup(
     name = 'depot',
-    version = '0.0.11-dev',
+    version = '0.0.11',
     packages = find_packages(),
     author = 'Noah Kantrowitz',
     author_email = 'noah@coderanger.net',
